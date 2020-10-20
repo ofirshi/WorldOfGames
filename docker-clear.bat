bd644dd0dc8fd6484baae7d7c5eea6f2f0ce2ae6
ofirshi <36884910+ofirshi@users.noreply.github.com>
2020-10-20 17:33:44 +0300

    aa

--- docker-clear.bat
+++ docker-clear.bat
@@ -1,3 +1,4 @@
 @echo off
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker kill %%i
 FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
 FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi %%i
