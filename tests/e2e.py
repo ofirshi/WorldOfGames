1b088f9652d827d510edf4e66580923a23b6d38e
ofirshi <36884910+ofirshi@users.noreply.github.com>
2020-10-20 09:41:10 +0300

    aa

--- tests/e2e.py
+++ tests/e2e.py
@@ -6,12 +6,25 @@ check that it is a number between 1 to 1000 and return a boolean value if it’s
 def test_scores_service(url, port) -> int:
     from selenium import webdriver
     from webdriver_manager.chrome import ChromeDriverManager
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    from selenium.webdriver.chrome.options import Options
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-logging')
+    chrome_options.add_argument('--no-default-browser-check')
+    chrome_options.add_argument('--no-first-run')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--enable-automation')
+
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
     try:
         driver.get("http://" + url + ":" + port)
         driver.implicitly_wait(5)  # wait up to
         actual = driver.find_element_by_id("score").text
+        # print(actual)
         if 1 <= int(actual) <= 1000:
             driver.close()
             return 0
@@ -38,7 +51,7 @@ if __name__ == "__main__":
     from sys import argv
 
     try:
-        print(len(argv))
+        # print(len(argv))
         if len(argv) == 3:
             param_1 = argv[1:]
             param_2 = argv[2:]
